edaa50e7e127e4d34cc8c41ca71f3c7dec99fbd3
Mel1Am <mel1am@hotmail.com>
2022-06-07 16:16:08 -0500

    New Table Student 3

--- SQL_BasicQueries.sql
+++ SQL_BasicQueries.sql
@@ -11,7 +11,6 @@ DESCRIBE student;
 --Command to print all the table
 SELECT * FROM STUDENT;
 
-
 --Command to delete table
 DROP TABLE student;
 
@@ -28,9 +27,14 @@ INSERT INTO student VALUES(2,'Kate','Sociology');
 --without getting an error for missing one column
 INSERT INTO student(student_id, name) VALUES(3,'Claire');
 
+--Insert more rows into table 1 named student
 INSERT INTO student VALUES (4,'Jack','Biology');
 INSERT INTO student VALUES (5,'Mike','Computer Science');
 
+----------------------------------------------------------------------------
+--Create a second table student2 with different config
+-- Not NULL: Value cannot have a NUll
+-- UNIQUE: only one row can have the same value
 create table student2 (
     student_id INT,
     name VARCHAR(20) NOT NULL,
@@ -38,3 +42,34 @@ create table student2 (
     PRIMARY KEY(student_id)
 );
 
+--Add rows to the table student2
+-- According to the config we stablished, major field cannot have the same VALUES
+-- In more than one row so it will throw an error
+-- also if you put Null on name it will throw an error
+
+INSERT INTO student2 VALUES(1,'Jack','Biology');
+INSERT INTO student2 VALUES(2,'Kate','Sociology');
+INSERT INTO student2 VALUES(3,'Claire','Chemistry');
+INSERT INTO student2 VALUES(4,'Jack','Biology');
+INSERT INTO student2 VALUES(5,'Mike','Computer Science');
+
+SELECT * FROM student2;
+
+------------------------------------------------------------------------
+--Create a third table student3 with different config
+-- AUTO_INCREMENT: automates the function for primary key instead of placing it manually
+-- DEFAULT: lets you established a default value
+CREATE table student3 (
+    student_id INT AUTO_INCREMENT,
+    name VARCHAR(20),
+    major VARCHAR(20) DEFAULT 'undecided',
+    PRIMARY KEY(student_id)
+);
+
+-- Inserting rows into the table student3
+INSERT INTO student3(name,major) VALUES('Jack','Biology');
+INSERT INTO student3(name,major) VALUES('Kate','Sociology');
+
+SELECT * FROM student3;
+
+drop Table student3;
